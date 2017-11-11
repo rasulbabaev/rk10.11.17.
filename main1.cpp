1320f97e66a0087345f5bc06a291382df2ccdda8
rasulbabaev <32385063+rasulbabaev@users.noreply.github.com>
2017-11-11 23:35:46 +0300

    Create main1.cpp

---
+++ main1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+int main()
+
+{
+	setlocale(LC_ALL, "Russian");
+	using namespace std;
+    int i, n, x;
+    cin >> n;
+    if (n <= 0)
+        return 0;
+    cout << "Вводите через пробел"<< endl;
+    
+    int a[n];
+    for (i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cin >> x;
+    int b[x - 1], c[n - x - 1];
+    for (i = 0; i < x; i++) {
+        b[i] = a[i];
+    }
+    for (i = 0; i < n - x; i++) {
+        c[i] = a[i + x];
+    }
+    for (i = 0; i < n - x; i++) {
+        cout << c[i] << " ";
+    }
+    for (i = 0; i < x; i++) {
+        cout << b[i] << " ";
+    }
+    return 0;
+}
