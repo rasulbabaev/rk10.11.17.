bc1e5ed35a61673dc740245f27427480e2508765
rasulbabaev <32385063+rasulbabaev@users.noreply.github.com>
2017-11-17 11:26:34 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,37 +1,44 @@
-#include <iostream>
-
-
-int main()
-
-{
-	setlocale(LC_ALL, "Russian");
-	using namespace std;
-    int i, n, x;
-    cin >> n;
-    if (n <= 0){
-	    cout << " error";
-        return 0;
-    }
-    cout << "Вводите через пробел"<< endl;
-    
-    int *a = new int[n];
-    for (i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    cin >> x;
-    int b[x - 1], c[n - x - 1];
-    for (i = 0; i < x; i++) {
-        b[i] = a[i];
-    }
-    for (i = 0; i < n - x; i++) {
-        c[i] = a[i + x];
-    }
-    for (i = 0; i < n - x; i++) {
-        cout << c[i] << " ";
-    }
-    for (i = 0; i < x; i++) {
-        cout << b[i] << " ";
-    }
-	delete [] a;
-    return 0;
+#include <iostream> 
+using namespace std; 
+ 
+int main() 
+{ 
+ int i, n, x; 
+ cout « "Введите размер последовательности: " « endl; 
+ cin » n; 
+ if (n <= 0) { 
+ cout « "error" « endl; 
+ return 1; 
+ } 
+ int *a = new int [n]; 
+ cout « "Введите элементы последовательности: " « endl; 
+ for (i = 0; i < n; i++) { 
+ cin » a[i]; 
+ } 
+ 
+ cin » x; 
+ if ( x <= 0) { 
+ cout « "error" « endl; 
+ return 1; 
+ } 
+ if ( x > n ) { 
+ cout « "error" « endl; 
+ return 1; 
+ } 
+ int b[x - 1], c[n - x - 1]; 
+ for (i = 0; i < x; i++) { 
+ b[i] = a[i]; 
+ } 
+ for (i = 0; i < n - x; i++) { 
+ c[i] = a[i + x]; 
+ } 
+ for (i = 0; i < n - x; i++) { 
+ cout « c[i] « " "; 
+ } 
+ for (i = 0; i < x; i++) { 
+ cout « b[i] « " "; 
+ } 
+ delete [] a; 
+ return 0; 
 }
+    
